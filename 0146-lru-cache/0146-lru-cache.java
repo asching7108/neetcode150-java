e674a3191659febebd30c6ce8f723f0316f9e024
asching7108 <53709035+asching7108@users.noreply.github.com>
2026-03-17 21:46:22 +0800

    Time: 48 ms (44.95%), Space: 133.9 MB (12.87%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,75 @@
+class ListNode {
+    int key;
+    int val;
+    ListNode next;
+    ListNode prev;
+
+    public ListNode(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+class LRUCache {
+    int capacity;
+    Map<Integer, ListNode> map;
+    ListNode head;
+    ListNode tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new ListNode(-1, -1);
+        tail = new ListNode(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        ListNode node = map.get(key);
+        remove(node);
+        add(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            ListNode oldNode = map.get(key);
+            remove(oldNode);
+        }
+
+        ListNode node = new ListNode(key, value);
+        map.put(key, node);
+        add(node);
+
+        if (map.size() > this.capacity) {
+            ListNode nodeToDelete = head.next;
+            remove(nodeToDelete);
+            map.remove(nodeToDelete.key);
+        }
+    }
+
+    private void add(ListNode node) {
+        ListNode prevEnd = tail.prev;
+        prevEnd.next = node;
+        node.prev = prevEnd;
+        node.next = tail;
+        tail.prev = node;
+    }
+
+    private void remove(ListNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
