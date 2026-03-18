059151f23a3bb8dc05c6e8d450eb0cda8e61210c
asching7108 <53709035+asching7108@users.noreply.github.com>
2026-03-18 23:10:15 +0800

    Time: 184 ms (22.63%), Space: 43.1 MB (40.58%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,18 +1,22 @@
 class Solution {
     private char[][] board;
+    private String word;
     private int ROWS;
     private int COLS;
     private int[][] directions;
 
     public boolean exist(char[][] board, String word) {
         this.board = board;
+        this.word = word;
         this.ROWS = board.length;
         this.COLS = board[0].length;
         this.directions = new int[][] { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
 
+        boolean[][] visited = new boolean[this.ROWS][this.COLS];
+
         for (int row = 0; row < this.ROWS; row++) {
             for (int col = 0; col < this.COLS; col++) {
-                if (this.backtrack(row, col, word, 0)) {
+                if (this.backtrack(row, col, 0, visited)) {
                     return true;
                 }
             }
@@ -21,8 +25,8 @@ class Solution {
         return false;
     }
 
-    private boolean backtrack(int row, int col, String word, int index) {
-        if (index >= word.length()) {
+    private boolean backtrack(int row, int col, int index, boolean[][] visited) {
+        if (index == word.length()) {
             return true;
         }
 
@@ -31,26 +35,28 @@ class Solution {
             row == this.ROWS ||
             col < 0 ||
             col == this.COLS ||
+            visited[row][col] ||
             this.board[row][col] != word.charAt(index)
         ) {
             return false;
         }
 
+        visited[row][col] = true;
         boolean res = false;
-        this.board[row][col] = '#';
 
         for (int[] d : this.directions) {
             if (this.backtrack(
                 row + d[0],
                 col + d[1],
-                word, index + 1
+                index + 1,
+                visited
             )) {
                 res = true;
                 break;
             }
         }
 
-        this.board[row][col] = word.charAt(index);
+        visited[row][col] = false;
         return res;
     }
 }
